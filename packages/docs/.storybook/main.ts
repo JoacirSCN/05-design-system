363f009a2b27dcc3f1fbfb7c55759a40977e4e0f
joacir <joacirprog@gmail.com>
2023-04-14 07:57:44 -0300

    fix: Add core components

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,37 +1,24 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
-
-
 const config: StorybookConfig = {
-  stories: [
-    "../src/**/*.mdx", 
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/addon-a11y', 
-
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", '@storybook/addon-a11y', '@storybook/addon-mdx-gfm'],
   framework: {
-    name: "@storybook/react-vite",
-    options: {
-    },
+    name: '@storybook/react-vite',
+    options: {}
   },
   docs: {
     autodocs: 'tag',
-    defaultName: 'Documentation',
+    defaultName: 'Documentation'
   },
-  core: {
-    builder: '@storybook/builder-vite'
-  },
-  viteFinal: async (config, { configType }) => {
+  core: {},
+  viteFinal: async (config, {
+    configType
+  }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/05-design-system/';
     }
-
-    return config
-  },
-
+    return config;
+  }
 };
 export default config;
