da09da6950a86467a269a372539a74e3bb8f2c89
joacir <joacirprog@gmail.com>
2023-04-14 08:13:14 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,8 +1,16 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", '@storybook/addon-a11y', '@storybook/addon-mdx-gfm'],
+  stories: [
+    "../src/**/*.mdx", 
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links", 
+    "@storybook/addon-essentials", 
+    "@storybook/addon-interactions", 
+    '@storybook/addon-a11y', 
+    '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/react-vite',
     options: {}
